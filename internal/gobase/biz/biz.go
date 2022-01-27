69e8a23dd9b79e4649f4f20d987a13effc125e6a
pines <dualpines@163.com>
2022-01-27 11:02:52 +0800

    clean code

--- internal/gobase/biz/biz.go
+++ internal/gobase/biz/biz.go
@@ -1,39 +1,39 @@
 package biz
 
 import (
+	"github.com/fizzse/gobase/internal/gobase/dao"
+
 	pbBasev1 "github.com/fizzse/gobase/protoc/gobase/v1"
 	"go.uber.org/zap"
-
-	"github.com/fizzse/gobase/internal/gobase/dao"
 )
 
 // 显示声明 SampleBiz 实现了Biz
-//var _ Biz = &SampleBiz{}
-//
-//// GinBiz http interface
-//type GinBiz interface {
-//	Ping(ginCtx *gin.Context)
-//	CreateUserGin(ginCtx *gin.Context)
-//}
-//
-//type Biz interface {
-//	GinBiz              // http server
-//	pbBase.GobaseServer // grpc
-//
-//	Close()
-//	CreateUser(ctx context.Context, user *CreateUserReq) (*UserInfo, error)
-//
-//	DealMsg(ctx context.Context, msg kafka.Event) error
-//}
+/*var _ Biz = &SampleBiz{}
 
-// New 返回抽象的接口
-//func New(daoCtx dao.Dao) (Biz, func(), error) {
-//	bizCtx := &SampleBiz{
-//		daoCtx: daoCtx,
-//	}
-//
-//	return bizCtx, bizCtx.Close, nil
-//}
+// GinBiz http interface
+type GinBiz interface {
+	Ping(ginCtx *gin.Context)
+	CreateUserGin(ginCtx *gin.Context)
+}
+
+type Biz interface {
+	GinBiz              // http server
+	pbBase.GobaseServer // grpc
+
+	Close()
+	CreateUser(ctx context.Context, user *CreateUserReq) (*UserInfo, error)
+
+	DealMsg(ctx context.Context, msg kafka.Event) error
+}
+
+//New 返回抽象的接口
+func New(daoCtx dao.Dao) (Biz, func(), error) {
+	bizCtx := &SampleBiz{
+		daoCtx: daoCtx,
+	}
+
+	return bizCtx, bizCtx.Close, nil
+}*/
 
 // NewInstance 返回具体实例
 func NewInstance(daoCtx *dao.SampleDao, logger *zap.SugaredLogger) (*SampleBiz, func(), error) {
--- internal/gobase/biz/worker.go
+++ internal/gobase/biz/worker.go
@@ -2,73 +2,10 @@ package biz
 
 import (
 	"context"
-	"encoding/json"
-	"sync"
-
-	"github.com/prometheus/client_golang/prometheus"
 
 	"github.com/fizzse/gobase/pkg/mq/kafka"
 )
 
-const (
-	namespace = "gobase" // serverName
-)
-
-var (
-	msgCounter     *prometheus.CounterVec
-	msgCounterOnce sync.Once
-)
-
-const (
-	statusOk     = "ok"
-	statusFailed = "failed"
-)
-
-func NewMetricCounter(name, help string, labels ...string) *metricCounter {
-	msgCounterOnce.Do(func() { // init in first call
-		msgCounter = prometheus.NewCounterVec(
-			prometheus.CounterOpts{
-				Namespace: namespace,
-				Name:      name, //   "process_msg_count",
-				Help:      help, // "Total number of message of iot device",
-			}, labels,
-		)
-	})
-
-	return &metricCounter{labels: labels, status: statusOk}
-}
-
-type metricCounter struct {
-	labels []string
-	status string
-}
-
-func (c *metricCounter) metric() {
-	lvs := c.labels
-	msgCounter.WithLabelValues(lvs...).Inc()
-}
-
-// DealMsg kafka
-
-type ExampleMsg struct {
-	Key string `json:"key"`
-}
-
 func (b *SampleBiz) DealMsg(ctx context.Context, msg kafka.Event) (err error) { // 回调函数
-	parsedMsg := ExampleMsg{}
-	err = json.Unmarshal(msg.Payload, &parsedMsg)
-	if err != nil {
-		return err
-	}
-
-	m := NewMetricCounter("process_msg_count", "Total number of message of iot device", parsedMsg.Key)
-
-	defer func() {
-		if err != nil {
-			m.status = statusOk
-		}
-
-		m.metric()
-	}()
 	return
 }
